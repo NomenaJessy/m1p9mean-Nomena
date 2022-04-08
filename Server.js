75c950653364cf0cac5cca431bfa7ed0def87f6b
NomenaJessy <nomenajessy.nj@gmail.com>
2022-04-08 20:03:03 +0300

    update Server.js

--- Server.js
+++ Server.js
@@ -17,9 +17,7 @@ app.use(profilRouter);
 app.use(commandeRouter);
 app.use(livraisonRouter);
 
-const port = process.env.PORT || 1042;
-
-app.listen(port,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server is running on port 1042");
 });
 
